10cb263a29d571aa454b412008fb6a461536460a
luba <lubo4ka.boiko@gmail.com>
2023-07-03 22:32:40 +0300

    redefine path

--- lib/utils/yarn_wrapper.rb
+++ lib/utils/yarn_wrapper.rb
@@ -4,7 +4,8 @@ module Utils
   module YarnWrapper
     def run_yarn(command, *args)
       sh_command = <<~SH
-        cd #{gem_path} && yarn #{Shellwords.escape(command)} #{prepare_args(args)}
+        export PATH='/home/deployer/.rvm/gems/ruby-3.1.2/bin:/home/deployer/.rvm/gems/ruby-3.1.2@global/bin:/home/deployer/.rvm/rubies/ruby-3.1.2/bin:/home/deployer/.rvm/bin:/home/deployer/.nvm/versions/node/v14.21.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin' &&
+        cd #{gem_path} && yarn #{Shellwords.escape(command)} #{prepare_args(args)} > /home/deployer/apps/changesync_dev/current/log/yarn.txt   2> /home/deployer/apps/changesync_dev/current/log/yarn_err.txt  
       SH
       Rails.logger.info("YARN: sh_command: #{sh_command}") if defined?(Rails) && Rails.logger
       result = system(sh_command)
